058b19b2641a6831dc5c307b2e8ac46ef6fee762
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-01 23:08:04 +0700

    suachoky

--- assigntment.cpp
+++ assigntment.cpp
@@ -4,10 +4,11 @@ in this problem , a natural number is given. The objective is to find a digit an
 in the number to obtain the smallest possible value
 Note: the smallest value must have the same number of digits as in the original value; thus,
 0 cannot be used
-    Example: 51209 -> the possible value is 15029 after one swap. 
-    Propose an algorithm using iteration and implement it in C++ to complete the task
-    Propose and implement an alternative algorithm using recusion for this task.
-    Calculate the complexity of the two approach. justify the answer, by making comment on the code
+    - Example: 51209 -> the possible value is 15029 after one swap. 
+    yêu cầu: 
+    - Propose an algorithm using iteration and implement it in C++ to complete the task
+    - Propose and implement an alternative algorithm using recusion for this task.
+    - Calculate the complexity of the two approach. justify the answer, by making comment on the code
 */
 
 /*
