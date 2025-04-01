fa16d3fa8c978c068b3d6a0a4727543df32ab69d
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-01 22:56:47 +0700

    full

---
+++ as3.cpp
@@ -0,0 +1,9 @@
+/*
+Given an algorithm of Odd and even sorting is a variant of bubble sort, tự tra code trên mạng nhé a lười type
+
+Yêu cầu đề này: 
+- initialize an array of the size n >= 6. Array values are initialized based on your choice. 
+- Explain the sorting process and display the result at each iteration of the sorting process.
+Note: this question does not require any implementation in C++. you need to make comments on your file. 
+
+*/
