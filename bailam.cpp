5ad87cb86f86017e2f42b857bbb4bc4bc8118207
Tony <iampt9524@gmail.com>
2025-04-01 22:58:37 +0700

    bai1test

--- bailam.cpp
+++ bailam.cpp
@@ -1,9 +1,26 @@
- #include <iostream>
- using namespace std;
+#include <iostream>
+#include <string>
+#include <algorithm>
 
- int main() {
+std::string smallestNumberIterative(std::string num) {
+    std::string result = num;
+    int n = num.length();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                std::string temp = num;
+                std::swap(temp[i], temp[j]);
+                if (temp[0] != '0' && temp < result) {
+                    result = temp;
+                }
+            }
+        }
+    }
+    return result;
+}
 
- int i=1;
- printf("ketqua %d", i);
-
- }
+int main() {
+    std::string num = "51209";
+    std::cout << "Result: " << smallestNumberIterative(num) << std::endl;
+    return 0;
+}
