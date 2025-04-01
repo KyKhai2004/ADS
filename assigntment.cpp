9d1dbae4e9ad96e87674351ce8c798a4508c368b
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-01 18:00:05 +0700

    debai

--- assigntment.cpp
+++ assigntment.cpp
@@ -1,6 +1,8 @@
 /*
 Ex1:
-
+excercise 1:
+1. Write a program that takes a string as input and prints the string in reverse order.
+2. Write a program that takes a string as input and counts the number of vowels in the string.
 
 
 */
