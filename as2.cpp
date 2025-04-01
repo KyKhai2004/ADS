1caeae2ef1da89b69cbcba4cc8eb34aed3093375
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-01 22:57:44 +0700

    fullde

---
+++ as2.cpp
@@ -0,0 +1,15 @@
+/*
+implement data structure in C++ to work on a list of 3D vectors. 
+- Propose a data structure for a 3D vector(x,y,z) with x,y,z are real
+- Propose a List data structure to store a collection of 3D vector (the list capacity,n, can be pre defined)
+- Write a function to calculate the Euclidean distance between a pair of vector in the list
+- Given two vector v1 = (x1,y1,z1) and v2= (x2,y2,z2) then their distance is expressed by: 
+    d(v1,v2) = sqrt((x1 -x2)ˆ2 + (y1-y2)ˆ2 +(z1-z2)ˆ2)
+
+- write a function to display the vectors in a list and the distance matrix. 
+Note: the distance and matrix is square and of the size nxn. Each element of the matrix (i,j) is the
+distance between two vectors Vi and Vj
+- Write a main function to test all the above functions.
+
+
+*/
