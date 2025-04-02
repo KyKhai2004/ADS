c6293d744f075cd4521b558d965b1a3287beccf9
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-02 16:17:21 +0700

    bai1

--- assigntment.cpp
+++ assigntment.cpp
@@ -1,7 +1,19 @@
 
 /*
-
+e1: 
+suppose an array of size >= 6 is given. its value must be initialized by intergers of your choice. 
+We want to arrange this array and output array so that perfect square numbers are first and then
+non - perfect square number. 
+Note: perfect square number are natural numbers whose square root is alse natural. 
+- implement the following function in C++ to sove above problem 
+    + write function to check whether a number is perfect square(Do not use math.h) (2pts)
+    + Write a function to re- arrange perfect square number'position in the array (3pts)
+    + Write a function to display an input array (2pts)
+    + Write a function to test all above functions. (2pts)
+- Calculate the complexity of your algorithm. Justify the answer( Comment directly in your source files) (1pts)
 */
+
+
 
 
 
