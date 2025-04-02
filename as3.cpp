59721ff966a30c54061f7c6eb6e46628949457e7
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-02 16:29:44 +0700

    de bai 3

--- as3.cpp
+++ as3.cpp
@@ -1,3 +1,15 @@
 /*
+ex3: 
+Given an array as follows: 
+X = [20,60,14,5,13,38,90]
+- Binary search tree(BST) is used to store the array numbers. Draw the tree structure for this 
+array X
+Note: the following format can be used to design your tree in a text file. In this format, you can
+replace A,B,...., and G with the array numbers. 
+- Once we have obtained the BST for X, we want to visit all nodes in this tree. If we use following algorithm,
+show the result. 
+    - Visit a node and display its value. 
+    - Recursively visit the right child node. 
+    - Recursively visit the left child node. 
 
 */
