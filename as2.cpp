341282af20729652577b3fd96add8523ca281298
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-02 10:54:58 +0700

    de bai 2

--- as2.cpp
+++ as2.cpp
@@ -1,15 +1,15 @@
 /*
-implement data structure in C++ to work on a list of 3D vectors. 
-- Propose a data structure for a 3D vector(x,y,z) with x,y,z are real
-- Propose a List data structure to store a collection of 3D vector (the list capacity,n, can be pre defined)
-- Write a function to calculate the Euclidean distance between a pair of vector in the list
-- Given two vector v1 = (x1,y1,z1) and v2= (x2,y2,z2) then their distance is expressed by: 
-    d(v1,v2) = sqrt((x1 -x2)ˆ2 + (y1-y2)ˆ2 +(z1-z2)ˆ2)
-
-- write a function to display the vectors in a list and the distance matrix. 
-Note: the distance and matrix is square and of the size nxn. Each element of the matrix (i,j) is the
-distance between two vectors Vi and Vj
-- Write a main function to test all the above functions.
+Given an array of bubble sort algorithm swaps repeatedly the adjacent elements if they are in wrong order (asceding order)
+We want to implement an improve version of this sorting algorithm. The idea is to sort both maximum(at the end)
+and minimum (at the begining) in one pass and the array is sorted from both ends. 
+     - Traverse from left to right and compare adjacent elements and the maximum is placed at 
+     right side. 
+     - Traverse from right to left and compare adjacent elements and the minimum is placed in 
+     left side. 
+     - Repeat the process until the array is sorted. 
+ Yeu cau (luu y dem yeu cau cho du y): 
+ - implement this algorithm in C++ 
+ - calculate the complexity of above algorithm, justify the answer. 
 
 
 */
