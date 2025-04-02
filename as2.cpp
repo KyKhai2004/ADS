b30e2d816ee4bdde4f218e8add594c126f396051
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-02 11:27:04 +0700

    reset

--- as2.cpp
+++ as2.cpp
@@ -1,15 +1,6 @@
 /*
-Given an array of bubble sort algorithm swaps repeatedly the adjacent elements if they are in wrong order (asceding order)
-We want to implement an improve version of this sorting algorithm. The idea is to sort both maximum(at the end)
-and minimum (at the begining) in one pass and the array is sorted from both ends. 
-     - Traverse from left to right and compare adjacent elements and the maximum is placed at 
-     right side. 
-     - Traverse from right to left and compare adjacent elements and the minimum is placed in 
-     left side. 
-     - Repeat the process until the array is sorted. 
- Yeu cau (luu y dem yeu cau cho du y): 
- - implement this algorithm in C++ 
- - calculate the complexity of above algorithm, justify the answer. 
+ex2: 
+
 
 
 */
--- assigntment.cpp
+++ assigntment.cpp
@@ -1,25 +1,7 @@
 
 /*
-initialize a random array of your choice. the current goal is to verify whether an array number is pentagon
-Note: pentagon number is calculated by following formula: 
-        f(n) = (n*(3n-1))/2
-Yeu cau:
-    - write pseudo code using Recursion(if neccessary, Iteration) to check all values in the given array 
-    are pentagon or not(2pts)
-    - Write a program in C++ to complete your proposed algorithm in the pseudo-code (8pts)
-    - calculate the complexity of the function(2pts)
+ex1: 
 
 */
 
-/*
-ex2: 
-asdsjawjdasbjcabjdsbawe
 
-*/
-
-/*
-Ex3:
-
-
-
-*/
