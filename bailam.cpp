d2b8e9e938c6d81ee297c904b405bc9d4cc5735c
TTungMaverix <tungpq.22ba13320@usth.edu.vn>
2025-04-02 11:05:49 +0700

    Update bailam.cpp

--- bailam.cpp
+++ bailam.cpp
@@ -1,56 +1,54 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+using namespace std;
 
-// Iterative Solution
-std::string smallestNumberIterative(std::string num) {
-    std::string result = num;
-    int n = num.length();
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {  // Chỉ cần thử j > i để tránh lặp lại
-            std::string temp = num;
-            std::swap(temp[i], temp[j]);
-            if (temp[0] != '0' && temp < result) {
-                result = temp;
-            }
-        }
-    }
-    return result;
-    // Time Complexity: O(n²) - Two loops try all pairs (n * n), simplified to swap attempts
-    // Space Complexity: O(n) - Space for 'result' and 'temp' strings
-}
-//test
-// Recursive Solution
-void findSmallestRecursive(const std::string& num, std::string& result, int start, bool swapped) {
-    if (swapped) {  // Nếu đã hoán đổi, kiểm tra kết quả
-        if (num[0] != '0' && num < result) {
-            result = num;
-        }
-        return;
-    }
-    if (start >= num.length()) return;  // Dừng khi vượt quá độ dài
-    for (int i = start; i < num.length(); i++) {
-        if (i != start) {
-            std::string temp = num;
-            std::swap(temp[start], temp[i]);
-            findSmallestRecursive(temp, result, start + 1, true);  // Sau khi hoán đổi
-        }
-        findSmallestRecursive(num, result, start + 1, false);  // Không hoán đổi
-    }
-}
+// pseudo code
+// Algorithm CheckPentagonalArray(arr):
+//     Function isPentagonal(num):
+//         n = 1
+//         while true:
+//             pent = (n * (3n - 1)) / 2
+//             if pent == num:
+//                 return true
+//             if pent > num:
+//                 return false
+//             n = n + 1
+    
+//     For each number in arr:
+//         if isPentagonal(number) == true:
+//             print number + " is pentagonal"
+//         else:
+//             print number + " is not pentagonal"
 
-std::string smallestNumberRecursive(std::string num) {
-    std::string result = num;
-    findSmallestRecursive(num, result, 0, false);
-    return result;
-    // Time Complexity: O(n²) - Tries all possible single swaps recursively
-    // Space Complexity: O(n) - Recursion stack depth and string storage
+
+
+bool isPentagonal(int num) {
+    int n = 1;
+    while (true) {
+        long long pent = (n * (3LL * n - 1)) / 2;
+        if (pent == num) {
+            return true;
+        }
+        if (pent > num) {
+            return false;
+        }
+        n++;
+    }
 }
 
 int main() {
-    std::string num = "51209";
-    printf("Original: %s\n", num.c_str());
-    printf("Iterative: %s\n", smallestNumberIterative(num).c_str());  // Expected: 15209
-    printf("Recursive: %s\n", smallestNumberRecursive(num).c_str());  // Expected: 15209
+    int arr[] = {5, 10, 12, 15, 22, 25};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Checking array elements:" << endl;
+    for (int i = 0; i < size; i++) {
+        if (isPentagonal(arr[i])) {
+            cout << arr[i] << " is a pentagonal number" << endl;
+        } else {
+            cout << arr[i] << " is not a pentagonal number" << endl;
+        }
+    }
+    
     return 0;
 }
+
+// complexity: O(1)
