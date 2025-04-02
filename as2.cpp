24872c6161d3e3abbb20e495e836005ec62baa17
KaiKhiii <KaiKhiii@my-loz.local>
2025-04-02 16:23:35 +0700

    de bai 2

--- as2.cpp
+++ as2.cpp
@@ -1,3 +1,12 @@
 /*
-
+We want to implement a Date data structure to manipulate time: 
+- A structe to store month(possible value[1;12]) and day ([1;31]) (1pts)
+- A function to initalize a Date variable with two valid values(moth, day)(1pts)
+- A function to increase a Date variable by one day. (2pts)
+- A function to calculate the difference between two Date variables(2pts)
+- Write main function to test all the above function (1pts)
 */
+
+
+
+
